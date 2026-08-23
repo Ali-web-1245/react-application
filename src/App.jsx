89875c6ea892620d2884fd63bc340fae0eeefb16
M.Ali <alirajput8091@gmail.com>
2026-08-23 15:47:12 -0700

    Update website

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Shop from "./pages/shop/Shop";
-import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import ProductDetails from "./pages/ProductDetail/ProductDetail";
 import Cart from "./pages/cart/Cart";
 import Checkout from "./pages/checkout/Checkout";
 export default function App() {
