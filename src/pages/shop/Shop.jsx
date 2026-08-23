1176da812aeb181aa6620351ede7724e92ad45dc
M.Ali <alirajput8091@gmail.com>
2026-08-23 15:34:53 -0700

    Update website

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
-import ShopLayout from "../../components/shopLayout/ShopLayout";
+import ShopLayout from "../../components/shoplayout/ShopLayout";
 import Newsletter from "../../components/newsletter/Newsletter";
 import Footer from "../../components/footer/Footer";
 
