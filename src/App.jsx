e498821f4efdabaa06ea5f79d82cef5090751a73
M.Ali <alirajput8091@gmail.com>
2026-08-23 15:26:50 -0700

    Fix paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Shop from "./pages/shop/Shop";
-import ProductDetails from "./pages/productdetails/ProductDetails";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
 import Cart from "./pages/cart/Cart";
 import Checkout from "./pages/checkout/Checkout";
 export default function App() {
